ed9ad59695129a686a471fdb71e8d0dc0e0aa1a9
akaishisho <90311696+akaishisho@users.noreply.github.com>
2021-10-22 10:56:43 +0000

    [add]修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+devise_for :users, controllers: {
+    sessions: 'devise/sessions',
+    registrations: 'devise/registrations'
+  }
+
+  root "homes#top"
+
+  get "/home/about" => "homes#about"
+  resources :users
+  resources :books
 end
