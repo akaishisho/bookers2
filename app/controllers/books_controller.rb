7703473848b28594afe5f7a4f88bedbea8888b22
akaishisho <90311696+akaishisho@users.noreply.github.com>
2021-10-23 06:43:08 +0000

    [Add]追加修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -38,7 +38,7 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
     @book.user_id = current_user.id
     if @book.update(book_params)
-      flash[:notice]="Book was successfully updated."
+      flash[:notice]="You have updated book successfully."
       redirect_to book_path(@book.id)
     else
       render :edit
@@ -47,10 +47,8 @@ class BooksController < ApplicationController
 
   def destroy
     @book = Book.find(params[:id])
-    if @book.destroy
-      flash[:notice]="Book was successfully destroyed."
-      redirect_to books_path
-    end
+    @book.destroy
+    redirect_to books_path
   end
 
   private
