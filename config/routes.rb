23c0bc84d7c05d4f982e3fcec101fe61116242ea
akaishisho <90311696+akaishisho@users.noreply.github.com>
2021-11-03 08:43:45 +0000

    [add]フォロー・フォロワー機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'relationships/followings'
+  get 'relationships/followers'
 devise_for :users, controllers: {
     sessions: 'devise/sessions',
     registrations: 'devise/registrations'
@@ -11,5 +13,11 @@ devise_for :users, controllers: {
   resources :books do
     resource :favorites,only:[:create,:destroy]
     resources :book_comments
+  end
+
+  resources :users do
+    resource :relationships,only: [:create,:destroy]
+    get 'followings' => 'relationships#followings',as: 'followings'
+    get 'followers' => 'relationships#followers',as: 'followers'
   end
 end
